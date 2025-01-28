7b9eb21e2e60d19957eeaf79c608596eefe9d0d1
Thalari <thalari@hotmail.com>
2025-01-28 06:58:32 +0530

    feat: Add number guessing game with input validation and exit option

--- examples/guess_number/guess_number.go
+++ examples/guess_number/guess_number.go
@@ -3,21 +3,43 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 )
 
 func main() {
 	fmt.Println("GUESS NUMBER")
+	fmt.Printf("Guess a number between 0 and 99\n")
 	fmt.Println("type 'exit' to quit")
 
-	b := rand.Intn(99)
+	b  := rand.Int63n(99)
 
-	var a int
-	fmt.Scanf("%d", &a)
+	var a_str string
+	fmt.Scanf("%s", &a_str)
+
+	if a_str == "exit" {
+		return
+	}
+
+	a, err := strconv.ParseInt(a_str, 10, 64)
+	if err != nil {
+		fmt.Println("Invalid input")
+	}
+	
 
 	for a != b {
 		fmt.Println("Wrong 🚫")
-		fmt.Scanf("%d", &a)
+		fmt.Scanf("%s", &a_str)
+		
+		if a_str == "exit" {
+			return
+		}
+		
+		a_temp, err := strconv.ParseInt(a_str, 10, 64)
+		if err != nil {
+			fmt.Println("Invalid input")
+		}
+		a = a_temp
 	}
 
-	fmt.Println("Correct 👍🏻")
+	fmt.Println("Correct ✅")
 }
